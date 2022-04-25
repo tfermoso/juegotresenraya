dcb8609c6cffb417117c99fc43799739f2e2ccdd
MrDomine <d.d.dominus1989@gmail.com>
2022-04-25 17:46:10 +0200

    creado el html del index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div id="login">
+        <form action="" method="post">
+            <label for="">Usuario</label>
+            <input type="text" name="nombre" id="" placeholder="Nombre de usuario">
+            <label for="">Contraseña</label>
+            <input type="password" name="pass" id="" placeholder="Contraseña">
+            <input type="submit" value="Entrar">
+            <span>Si no estas registrado puedes hacerlo <a href="registro.php">aquí</a></span>
+        </form>
+    </div>
+</body>
+</html>
