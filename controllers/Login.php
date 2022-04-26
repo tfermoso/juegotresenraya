046de2fa29f1f36d8f22021f913626ef4e330e43
tfermoso <tomas.fermoso@gmail.com>
2022-04-26 16:58:00 +0200

    base modelo mvc

---
+++ controllers/Login.php
@@ -0,0 +1,16 @@
+<?php
+    class LoginController{
+
+        public function __construct()
+        {
+         
+        }
+
+        public function index()
+        {
+            
+            require_once("views/login/login.php");
+        }
+    }
+
+?>
--- index.php
+++ index.php
@@ -1,49 +1,27 @@
 <?php
-require("config/database.php");
-session_start();
+require("config/Config.php");
+require("core/router.php");
 
-if (isset($_POST["usuario"])) {
-    $nombre = $_POST["nombre"];
-    $usuario = $_POST["usuario"];
-    $password = $_POST["password"];
-    $conn = Conectar::conexion();
-    //$consultaLogin="select * form usuario where idusuario=? and password=?";
-    $consultaInsert = "insert into usuario (nombre,usuario,password) value (?,?,?)";
-    $stm = $conn->prepare($consultaInsert);
-    $stm->bind_param("sss", $nombre, $usuario, $password);
-    try {
-        //code...
-        $stm->execute();
-        $resultado = $stm->get_result();
-        if ($resultado) {
-            var_dump($resultado);
+if(isset($_GET['c'])){
+		
+    $controlador = cargarControlador($_GET['c']);
+    
+    if(isset($_GET['a'])){
+        if(isset($_GET['id'])){
+            cargarAccion($controlador, $_GET['a'], $_GET['id']);
+            } else {
+            cargarAccion($controlador, $_GET['a']);
         }
-    } catch (\Throwable $th) {
-        $error=$th->getMessage();
+        } else {
+        cargarAccion($controlador, ACCION_PRINCIPAL);
     }
- 
+    
+    } else {
+    
+    $controlador = cargarControlador(CONTROLADOR_PRINCIPAL);
+    $accionTmp = ACCION_PRINCIPAL;
+    $controlador->$accionTmp();
 }
 
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <form action="" method="post">
-        <input type="text" name="nombre" placeholder="nombre">
-        <input type="text" name="usuario" id="" placeholder="usuario">
-        <input type="password" name="password" id="" placeholder="password">
-        <input type="submit" value="Nuevo Usuario">
-    </form>
-    <p><?php if(isset($error)) echo $error; ?></p>
-</body>
-
-</html>
