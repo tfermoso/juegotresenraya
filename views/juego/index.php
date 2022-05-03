04023b76c88a9ed9b252068ce76736007c702104
cristianCmon <cristianXP@protonmail.com>
2022-05-03 15:18:19 +0200

    primer commit

--- views/juego/index.php
+++ views/juego/index.php
@@ -8,12 +8,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
     <script src="assets/js/jquery-3.6.0.min.js"></script>
-   <script src="assets/js/bootstrap.min.js"></script>
-   <script src="./assets/js/juego.js"></script>
-
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="./assets/js/juego.js"></script>
 
     <title>Tres en Raya</title>
 </head>
