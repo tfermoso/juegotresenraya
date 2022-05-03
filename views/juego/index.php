3b5f29875b0fcf7a63f84cb2012dde679cb2100a
tfermoso <tomas.fermoso@gmail.com>
2022-05-03 17:20:25 +0200

    merge

--- views/juego/index.php
+++ views/juego/index.php
@@ -9,19 +9,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< HEAD
-=======
-    
->>>>>>> 04023b76c88a9ed9b252068ce76736007c702104
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
     <script src="assets/js/jquery-3.6.0.min.js"></script>
     <script src="assets/js/bootstrap.min.js"></script>
     <script src="./assets/js/juego.js"></script>
-<<<<<<< HEAD
-
-=======
->>>>>>> 04023b76c88a9ed9b252068ce76736007c702104
 
     <title>Tres en Raya</title>
 </head>
